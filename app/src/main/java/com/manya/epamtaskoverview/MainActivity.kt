5fc8e699cc768322d94ff9f9d273b3d1e457761e
Maria Kirdun <manya.gumil.com>
2019-04-12 23:24:55 +0300

    Change code style.

--- app/src/main/java/com/manya/epamtaskoverview/MainActivity.kt
+++ app/src/main/java/com/manya/epamtaskoverview/MainActivity.kt
@@ -10,32 +10,46 @@ import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+/**
+ * This is main activity. We can choose image from gallery when click on button
+ * and picked image we can see on ImageView.
+ *
+ * @author: Maria Kirdun
+ *
+ */
 
-    val GALLERY_REQUEST = 1
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        OverviewButton.setOnClickListener {
-            val photoIntent = Intent(Intent.ACTION_PICK)
-            photoIntent.type = "image/*"
-            startActivityForResult(photoIntent, GALLERY_REQUEST)
+        chooseImageButton.setOnClickListener {
+          chooseImage()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == GALLERY_REQUEST) {
-            if (resultCode == Activity.RESULT_OK) {
+        when (requestCode) {
+            GALLERY_REQUEST -> if (resultCode == Activity.RESULT_OK) {
                 val uri = data?.data
                 val selectedImage = MediaStore.Images.Media.getBitmap(contentResolver,uri)
-                OverviewImageView.setImageBitmap(selectedImage)
+                pickedImageView.setImageBitmap(selectedImage)
             }
-        } else {
-            Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show()
+            else -> Toast.makeText(this, R.string.wrong_message, Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun chooseImage(){
+        val photoIntent = Intent(Intent.ACTION_PICK)
+        photoIntent.type = PHOTO_TYPE
+        startActivityForResult(photoIntent, Companion.GALLERY_REQUEST)
+    }
+
+    companion object {
+        private const val GALLERY_REQUEST : Int = 1
+        private const val PHOTO_TYPE : String = "image/*"
     }
 }
